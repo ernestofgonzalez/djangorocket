3bad1e9670660959eda1115711f5bb20e8bd918d
Ernesto F. González <ernestogonzalezz@yahoo.com>
2025-04-03 22:59:46 +0100

    Add `init`

--- djangorocket/cli.py
+++ djangorocket/cli.py
@@ -1,1 +1,11 @@
 import click
+from cookiecutter.main import cookiecutter
+
+@click.group()
+def main():
+    pass
+
+@main.command()
+def init():
+    """Run the cookiecutter template in the root folder."""
+    cookiecutter("./templates/base")
