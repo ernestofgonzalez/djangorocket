435807c0ab4eddafdba25df7c3cc3d4ec6b2c539
Ernesto F. González <ernestogonzalezz@yahoo.com>
2025-04-05 12:30:45 +0100

    Fix console script path

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'djangorocket = djangorocket.cli:cli',
+            'djangorocket = djangorocket.cli:main',
         ]
     },
     url="https://github.com/ernestofgonzalez/djangorocket",
