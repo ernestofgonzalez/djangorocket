ad30e953cd92c713be5ac7ab4f05a7d17427d417
Ernesto F. González <ernestogonzalezz@yahoo.com>
2025-04-17 18:50:24 +0200

    Read template data files

--- djangorocket/components.py
+++ djangorocket/components.py
@@ -1,4 +1,12 @@
+import importlib
+
+from cookiecutter.main import cookiecutter
 
 
-def add_components(components):
-    print("components", components)
+def add_components(components, templates_dir=None):
+    for component_name in components:
+        # Locate the zipped template in the installed package
+        dr_template_filepath = "templates/ui/accordion.zip"
+
+        with importlib.resources.path("djangorocket.templates.ui", "accordion.zip") as dr_template_fspath:
+            cookiecutter(str(dr_template_fspath), output_dir=templates_dir)
