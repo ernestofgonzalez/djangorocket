be3f92d5bdd2ce2bd6045d6c912732318d15209f
Ernesto F. González <ernestogonzalezz@yahoo.com>
2025-04-17 18:50:45 +0200

    Read templates dir

--- djangorocket/cli.py
+++ djangorocket/cli.py
@@ -2,11 +2,11 @@ import click
 from cookiecutter.main import cookiecutter
 
 from djangorocket.components import add_components
+from djangorocket.django import DjangoSettingsManager
 
 @click.group()
 def main():
     pass
-
 
 @main.command()
 def init():
@@ -15,6 +15,14 @@ def init():
 
 @main.command()
 @click.argument("components", nargs=-1)
-def add(components):
+@click.option("--templates-dir", default=None, help="Directory where the new template source file should be added to.")
+def add(components, templates_dir):
     """Add a UI cookiecutter template to an existing DjangoRocket project."""
-    add_components(components)
+    try:
+        if templates_dir is None:
+            django_settings = DjangoSettingsManager()
+            templates_dir = django_settings.get_templates_dir()
+
+        add_components(components, templates_dir)
+    except Exception as e:
+        click.echo(f"Error: {e}")
